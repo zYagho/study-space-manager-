8dce84e7cf819b4964a4e3010daa053b83ee2fde
ruanazevedo <ruannzeroh@gmail.com>
2025-02-20 12:26:38 -0300

    Footer criado

--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -1,6 +1,21 @@
+import { FaGithub } from 'react-icons/fa'
+import { GoLaw } from 'react-icons/go'
+
+import styles from './Footer.module.css'
+
 function Footer() {
     return (
-        <p>Rodapé</p>
+        <footer className={styles.footer}>
+            <p> &copy; {new Date().getFullYear()} Study Space Manager. Todos os direitos reservados.</p>
+            <p className={styles.links}>
+                <div>
+                    <FaGithub /><a href='https://github.com/zyagho'>Repositório do projeto</a>
+                </div> |
+                <div>
+                    <GoLaw /><a href='https://opensource.org/license/mit'>MIT License</a>
+                </div>
+            </p>
+        </footer>
     )
 }
 
