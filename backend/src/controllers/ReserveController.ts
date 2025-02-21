d1844af1459bb31d55988e370cfa6e35a71c06f0
Yagho Petini <yaghopetini@gmail.com>
2025-02-21 18:07:40 -0300

    Cadastro de reserva implementado. #23

--- backend/src/controllers/ReserveController.ts
+++ backend/src/controllers/ReserveController.ts
@@ -1,3 +1,80 @@
 import { Request, Response } from "express";
-import { AdminService } from "../services/AdminService";
+import { ReserveService } from "../services/ReserveService";
 
+
+class ReserveController{
+
+    private reserveService: ReserveService
+
+    constructor(){
+        this.reserveService = new ReserveService()
+    }
+
+    async create(req: Request, res: Response){
+
+        try{
+            const {userEmail} = req.body
+
+            const reserve = await this.reserveService.create({userEmail})
+
+            return res.status(200).json(reserve)
+        }catch(err){
+            return res.status(400).json({Error:err.message})
+        }
+    }
+
+    async update(req: Request, res: Response){
+
+        // try{
+        //     const {userEmail} = req.body
+
+        //     const reserve = await this.reserveService.create({userEmail})
+
+        //     return res.status(200).json(reserve)
+        // }catch(err){
+        //     return res.status(400).json({Error:err.message})
+        // }
+    }
+
+    async delete(req: Request, res: Response){
+
+        try{
+            const {id} = req.params
+
+            const reserve = await this.reserveService.delete({id})
+
+            return res.status(200).json(reserve)
+        }catch(err){
+            return res.status(400).json({Error:err.message})
+        }
+    }
+
+    async list(req: Request, res: Response){
+
+        try{
+
+            const reserve = await this.reserveService.list()
+
+            return res.status(200).json(reserve)
+        }catch(err){
+            return res.status(400).json({Error:err.message})
+        }
+    }
+
+    
+    async listReserveUser(req: Request, res: Response){
+
+        try{
+
+            const {userEmail} = req.body
+
+            const reserve = await this.reserveService.listReserveUser({userEmail})
+
+            return res.status(200).json(reserve)
+        }catch(err){
+            return res.status(400).json({Error:err.message})
+        }
+    }
+}
+
+export {ReserveController}
