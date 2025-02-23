e870fea9716326fec265d2671de7cd8c2ba2d2c4
ruanazevedo <ruannzeroh@gmail.com>
2025-02-23 18:16:31 -0300

    correção de listagem de salas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -31,10 +31,10 @@ router.delete('/admin', isAuthenticated, isAdmin, adminController.delete.bind(ad
 //Rotas Room
 const roomController = new RoomController();
 router.post('/room', isAuthenticated, isAdmin, roomController.create.bind(roomController))
-router.get('/room', isAuthenticated, isAdmin, roomController.list.bind(roomController))
+router.get('/room', roomController.list.bind(roomController))
 router.delete('/room/:id', isAuthenticated, isAdmin, roomController.delete.bind(roomController))
 router.put('/room/:id', isAuthenticated, isAdmin, roomController.update.bind(roomController))
-router.get('/room/actives', isAuthenticated, isAdmin, roomController.listActiveRooms.bind(roomController))
+router.get('/room/actives', roomController.listActiveRooms.bind(roomController))
 
 const timesController = new TimesController();
 router.post('/time', isAuthenticated, isAdmin, timesController.create.bind(timesController))
