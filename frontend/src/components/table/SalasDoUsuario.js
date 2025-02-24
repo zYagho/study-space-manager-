11e6cc74d83a33f89a2e559c010a41a8b5b93dd8
ruanazevedo <ruannzeroh@gmail.com>
2025-02-23 21:00:07 -0300

    issue #5 - resumo de salas do usuário estilizado

--- frontend/src/components/table/SalasDoUsuario.js
+++ frontend/src/components/table/SalasDoUsuario.js
@@ -4,7 +4,44 @@ function SalasDoUsuario() {
     return (
         <div className={styles.salas_do_usuario}>
             <h3>Suas salas</h3>
-            <p>Suas salas serão exibidas aqui</p>
+            <ol className={styles.lista_dias}>
+                <li>Segunda 24/02/2025 
+                    <ul>
+                        <li>
+                            <ul className={styles.lista_salas} type="circle">
+                                <p>Sala 3:</p>
+                                <li>intervalo</li>
+                                <li>intervalo</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul className={styles.lista_salas}>
+                                <p>Sala 5:</p>
+                                <li>intervalo</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>Terça 25/02/2025
+                    <ul>
+                        <li>
+                            <ul className={styles.lista_salas}>
+                                <p>Sala 3:</p>
+                                <li>intervalo</li>
+                                <li>intervalo</li>
+                                <li>intervalo</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul className={styles.lista_salas}>
+                                <p>Sala 4:</p>
+                                <li>intervalo</li>
+                                <li>intervalo</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ol>
         </div>
     )
 }
