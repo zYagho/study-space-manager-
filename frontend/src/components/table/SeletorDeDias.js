bc42cee291bd7f4b1ee52427014e000926902479
ruanazevedo <ruannzeroh@gmail.com>
2025-02-23 22:57:38 -0300

    issue #5 - seletor de dias recebe a data atual

--- frontend/src/components/table/SeletorDeDias.js
+++ frontend/src/components/table/SeletorDeDias.js
@@ -1,10 +1,22 @@
 import styles from './SeletorDeDias.module.css'
 
 function SeletorDeDias() {
+
+    const currentDate = new Date()
+
+    const day = String(currentDate.getDate()).padStart(2, '0')
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0')
+    const year = currentDate.getFullYear()
+
+    // Formata a data no formato "dd/MM/yyyy"
+    const currentDateFormatted = `${day}/${month}/${year}`
+
+    const weekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+
     return (
         <div className={styles.day_selector}>
             <p>Anterior</p>
-            <h3>Segunda-feira 24/02/2025</h3>
+            <h3>{weekDays[currentDate.getDay()]} {currentDateFormatted}</h3>
             <p>Próximo</p>
         </div>
     )
