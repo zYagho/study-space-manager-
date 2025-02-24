21bc93b08e61cf1013e0f4e9cf3d5da2e630ccab
ruanazevedo <ruannzeroh@gmail.com>
2025-02-23 22:35:09 -0300

    issue #5 - ajustado função do clique das celulas

--- frontend/src/components/table/Cell.js
+++ frontend/src/components/table/Cell.js
@@ -4,10 +4,14 @@ import styles from './Cell.module.css'
 
 function Cell({ hours, room }) {
 
+    function onCellClick(hour) {
+        alert(`Sala ${room.number} e horário ${hour.horaInicio} - ${hour.horaFim}`)
+    }
+
     return (
         <>
             {hours.map((h) => (
-                <td key={h.id} className={styles.cell} onClick={() => alert(`Sala ${room.number} e horário ${h.horaInicio} - ${h.horaFim}`)}>{/* espaço para clicar */}</td>
+                <td key={h.id} className={styles.cell} onClick={() => onCellClick(h)}>{/* espaço para clicar */}</td>
             ))}
         </>
     )
